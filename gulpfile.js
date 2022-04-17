1218646bbacf433271cb786accbfb28c3032c7a2
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-04-17 16:54:42 +0100

    autoprefixer function

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
 
 // Create functions
 function compilaSass(){
     return gulp.src('scss/*.scss') // get all files from folder scss
     .pipe(sass({outputStyle : 'compressed'})) //minified css
+    .pipe(autoprefixer({
+        overrideBrowserlist: ['last 2 versions'], // code will work up to 2 last versions browsers
+        cascade: false
+    })) // generate all autoprefixer that exists
     .pipe(gulp.dest('css/')) //destination
 }
 
