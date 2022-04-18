534d9ac2fbe69aef6b4fdcf5968dbe95f39760b9
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-04-18 13:31:08 +0100

    installing concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create(); // create local server
+const concat = require('gulp-concat');
 
 
 // Create functions
+// Compile Sass Function
 function compilaSass(){
     return gulp.src('scss/*.scss') // get all files from folder scss
     .pipe(sass({outputStyle : 'compressed'})) //minified css
@@ -13,9 +15,15 @@ function compilaSass(){
         cascade: false,
     })) 
     .pipe(gulp.dest('css/')) // destination folder
+    .pipe(browserSync.stream()); // inject css into the page
 }
 gulp.task('default', compilaSass); // task (need a name)  default for all execute
+// Concat Function
 
+function gulpJS(){
+    return gulp.src()
+}
+// Browser Function
 function browser(){
     browserSync.init({
         server: {
@@ -23,9 +31,11 @@ function browser(){
         }
     })
 }
-gulp.task('default', browser);
-
+gulp.task('browser-sync', browser);
+// Watch Function
 function watch(){
     gulp.watch('scss/*.scss', compilaSass); // gulp.series('name-of-task') or use parrallel
+    gulp.watch('*.html').on('change', browserSync.reload); // refresh html when changes made
 }
-gulp.task('default', watch);
+gulp.task('watch', watch);
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
